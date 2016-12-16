e6745978e2cf78c716fa23d511d077bd6064ead8
Siftor <plumbdav@gmail.com>
2016-12-16 15:22:13 +0000

    Add project files.

---
+++ WebCW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCW.Startup))]
+namespace WebCW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
